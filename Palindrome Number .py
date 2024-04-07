01268798a3decc390a9516574f6792555b2618a2
hannanigussie <hniguse95@gmail.com>
2024-04-07 04:58:05 +0300

    palindrome

--- Palindrome Number .py
+++ Palindrome Number .py
@@ -1,5 +1,9 @@
-def isPalindrome(x: int) -> bool:
-    return str(x) == str(x)[::-1]
-
-num = int(input())
-print(isPalindrome(num))
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x and x % 10 == 0):
+            return False
+        y = 0
+        while y < x:
+            y = y * 10 + x % 10
+            x //= 10
+        return x in (y, y // 10)
