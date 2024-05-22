7a052fe41107a4e66d59e314d14dd60a5a799925
hannanigussie <hniguse95@gmail.com>
2024-05-22 20:10:22 +0300

    counting sort

---
+++ Counting Sort.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Initialize a frequency array of size 100 with all zeros
+    frequency = [0] * 100
+    
+    # Count the occurrences of each value in the input array
+    for num in arr:
+        frequency[num] += 1
+    
+    # Return the frequency array
+    return frequency
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
