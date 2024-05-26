f186dd791539f8681e5576e5c5756ce984c6f992
hannanigussie <hniguse95@gmail.com>
2024-05-26 13:32:39 +0300

    minimum common value

---
+++ Minimum Common Value.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        pointer1, pointer2 = 0, 0
+        
+        while pointer1 < len(nums1) and pointer2 < len(nums2):
+            if nums1[pointer1] == nums2[pointer2]:
+                return nums1[pointer1]
+            elif nums1[pointer1] < nums2[pointer2]:
+                pointer1 += 1
+            else:
+                pointer2 += 1
+        
+        return -1
